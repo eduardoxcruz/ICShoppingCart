5faf373901f3044a1d3e61cacbc31ccee6201290
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-23 21:50:48 -0600

    Add constructors

--- ShoppingCart/ViewModel/RelayCommand.cs
+++ ShoppingCart/ViewModel/RelayCommand.cs
@@ -4,6 +4,16 @@ namespace ShoppingCart.ViewModel;
 
 public class RelayCommand
 {
-	private readonly Action<object> _execute;  
-	private readonly Predicate<object> _canExecute; 
+	private readonly Action<object> _execute;
+	private readonly Predicate<object> _canExecute;
+
+	public RelayCommand(Action<object> execute) : this(execute, null)
+	{
+	}
+
+	public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+	{
+		_execute = execute ?? throw new ArgumentNullException(nameof(execute));
+		_canExecute = canExecute;
+	}
 }
