6b5afdff20bcb5eab9c4ef29470b0873edacb630
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-23 21:48:40 -0600

    Add private properties

--- ShoppingCart/ViewModel/RelayCommand.cs
+++ ShoppingCart/ViewModel/RelayCommand.cs
@@ -1,6 +1,9 @@
-﻿namespace ShoppingCart.ViewModel;
+﻿using System;
+
+namespace ShoppingCart.ViewModel;
 
 public class RelayCommand
 {
-	
+	private readonly Action<object> _execute;  
+	private readonly Predicate<object> _canExecute; 
 }
