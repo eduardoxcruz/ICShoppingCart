0bceedf84ccd344f8d9f770e4b57b2515e7b4e25
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-23 22:07:16 -0600

    Refactorize and implement ICommand interface

--- ShoppingCart/ViewModel/RelayCommand.cs
+++ ShoppingCart/ViewModel/RelayCommand.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using System.Windows.Input;
 
 namespace ShoppingCart.ViewModel;
 
-public class RelayCommand
+public class RelayCommand : ICommand
 {
-	private readonly Action<object> _execute;
-	private readonly Predicate<object> _canExecute;
+	private Action<object> _execute;
+	private Func<object, bool> _canExecute;
 
-	public RelayCommand(Action<object> execute) : this(execute, null)
+	public event EventHandler? CanExecuteChanged
 	{
+		add { CommandManager.RequerySuggested += value; }
+		remove { CommandManager.RequerySuggested -= value; }
 	}
 
-	public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+	public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null!)
 	{
 		_execute = execute ?? throw new ArgumentNullException(nameof(execute));
 		_canExecute = canExecute;
+	}
+
+	public bool CanExecute(object parameter)
+	{
+		return _canExecute == null || _canExecute(parameter);
+	}
+
+	public void Execute(object parameter)
+	{
+		_execute(parameter);
 	}
 }
