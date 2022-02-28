9b495d1d800fe050e4a68e0839baaf4b99b746b7
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-28 12:15:40 -0600

    Add ProductToBuySimpleView.xaml

---
+++ ShoppingCart/View/ProductToBuySimpleView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ShoppingCart.View;
+
+public partial class ProductToBuySimpleView : UserControl
+{
+	public ProductToBuySimpleView()
+	{
+		InitializeComponent();
+	}
+}
