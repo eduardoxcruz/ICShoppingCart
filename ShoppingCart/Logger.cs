0cffe3ffecce15f367e366099ae540d788a89e1e
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-03-03 11:29:53 -0600

    Add CreateLogger() method

--- ShoppingCart/Logger.cs
+++ ShoppingCart/Logger.cs
@@ -1,6 +1,21 @@
-﻿namespace ShoppingCart;
+﻿using Serilog;
+using SerilogUtils;
+
+namespace ShoppingCart;
 
 public static class Logger
 {
-	
+	public static ILogger CreateLogger<T>() where T : new()
+	{
+		string consoleOutputTemplate = 
+			"[{Timestamp:dd/mm/yyyy HH:mm:ss} {Level:u4} {SourceContext}.{MemberName}:{LineNumber}] {Message:lj}{NewLine}{Exception}";
+
+		return new LoggerConfiguration()
+			.MinimumLevel.Verbose()
+			.Enrich.FromLogContext()
+			.WriteTo.Console(outputTemplate: consoleOutputTemplate)
+			.WriteTo.File(new CompactJsonFormatter(), "log.txt", rollingInterval: RollingInterval.Day)
+			.CreateLogger()
+			.ForContext<T>();
+	}
 }
