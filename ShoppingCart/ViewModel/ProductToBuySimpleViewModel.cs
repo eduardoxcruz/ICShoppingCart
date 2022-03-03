36f5aa5467f3217a4c3f990ca3e15b4cf21db1f3
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-03-03 14:05:41 -0600

    Add calling methods to commands

--- ShoppingCart/ViewModel/ProductToBuySimpleViewModel.cs
+++ ShoppingCart/ViewModel/ProductToBuySimpleViewModel.cs
@@ -1,6 +1,10 @@
-﻿using Mux;
+﻿using System;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Mux;
 using Mux.Model;
 using Serilog;
+using SerilogUtils;
 using ShoppingCart.Model;
 
 namespace ShoppingCart.ViewModel;
@@ -28,5 +32,88 @@ public class ProductToBuySimpleViewModel : ViewModelBase
 	{
 		Context = new ICContext();
 		_productToBuy = new ProductToBuy();
+		AddProductToBuyCommand = new RelayCommand(AddProductToBuy, _ => true);
+		UpdateProductToBuyCommand = new RelayCommand(UpdateProductToBuy, _ => true);
+	}
+
+	private async void AddProductToBuy(object o)
+	{
+		if (!ConfirmAction("¿Desea agregar el registro?")) return;
+		
+		try
+		{
+			ProductToBuy newProductToBuy = GenerateNewProductToBuyToInsert();
+			ProductToBuy = new ProductToBuy();
+			newProductToBuy.Id = 0;
+			Context.ShoppingCart.Add(newProductToBuy);
+			await Context.SaveChangesAsync();
+			MyMessageBox.ShowSuccessBox("Exito al agregar el registro.");
+		}
+		catch (Exception exception)
+		{
+			Logger
+				.Here()
+				.Error(
+					"Ocurrio una excepcion al tratar de agregar un nuevo registro a Shopping Cart. {@BaseException}", 
+					exception.GetBaseException());
+			MyMessageBox.ShowErrorBox("Ocurrio un error al agregar el registro. Vea el log para mas detalles.");
+		}
+	}
+
+	private ProductToBuy GenerateNewProductToBuyToInsert()
+	{
+		ProductToBuy productToBuy = new()
+		{
+			RequestDate = DateTime.Now,
+			Status = "PENDIENTE"
+		};
+
+		if (ProductToBuy.Id > 0)
+		{
+			CopyModifiedProperties(ref productToBuy);
+		}
+
+		return productToBuy;
+	}
+
+	private void CopyModifiedProperties(ref ProductToBuy productToBuy)
+	{
+		productToBuy.Comments = ProductToBuy.Comments;
+		productToBuy.PetitionerId = ProductToBuy.PetitionerId;
+		productToBuy.InternalReference = ProductToBuy.InternalReference;
+		productToBuy.ProductDescription = ProductToBuy.ProductDescription;
+		productToBuy.RequestedAmount = ProductToBuy.RequestedAmount;
+		productToBuy.MountingTechnology = ProductToBuy.MountingTechnology;
+		productToBuy.EncapsulationType = ProductToBuy.EncapsulationType;
+		productToBuy.Priority = ProductToBuy.Priority;
+	}
+
+	private async void UpdateProductToBuy(object o)
+	{
+		if (ProductToBuy.Id == 0 || !ConfirmAction("¿Desea actualizar el registro?"))
+		{
+			return;
+		}
+		
+		try
+		{
+			Context.Entry(ProductToBuy).State = EntityState.Modified;
+			await Context.SaveChangesAsync();
+			MyMessageBox.ShowSuccessBox("Exito al actualizar el registro.");
+		}
+		catch (Exception exception)
+		{
+			Logger
+				.Here()
+				.Error(
+					"Ocurrio una excepcion al tratar de actualizar el registro en Shopping Cart. {@BaseException}", 
+					exception.GetBaseException());
+			MyMessageBox.ShowErrorBox("Ocurrio un error al actualizar el registro. Vea el log para mas detalles.");
+		}
+	}
+
+	private bool ConfirmAction(string confirmationMessage)
+	{
+		return MyMessageBox.ShowConfirmationBox(confirmationMessage) != MessageBoxResult.No;
 	}
 }
