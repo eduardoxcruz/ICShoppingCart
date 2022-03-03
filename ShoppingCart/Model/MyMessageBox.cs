522e880b5c6929bc59486b91f4ae10a20212cb09
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-03-03 13:27:22 -0600

    Add ShowConfirmationBox method

--- ShoppingCart/Model/MyMessageBox.cs
+++ ShoppingCart/Model/MyMessageBox.cs
@@ -1,6 +1,14 @@
-﻿namespace ShoppingCart.Model;
+﻿using System.Windows;
+
+namespace ShoppingCart.Model;
 
 public static class MyMessageBox
 {
-	
+	public static MessageBoxResult ShowConfirmationBox(string message)
+	{
+		return MessageBox.Show(message,
+			"Confirmar Accion",
+			MessageBoxButton.YesNo,
+			MessageBoxImage.Question);
+	}
 }
