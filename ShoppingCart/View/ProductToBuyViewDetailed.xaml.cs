f0d994da2fd9f7758256651cba3fd59576c288ef
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-03-16 17:03:43 -0600

    Delete ProductToBuyViewDetailed.xaml

---
+++ ShoppingCart/View/ProductToBuyViewDetailed.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ShoppingCart.View
+{
+	/// <summary>
+	/// Lógica de interacción para ProductToBuyViewDetailed.xaml
+	/// </summary>
+	public partial class ProductToBuyViewDetailed : UserControl
+	{
+		public ProductToBuyViewDetailed()
+		{
+			InitializeComponent();
+		}
+	}
+}
